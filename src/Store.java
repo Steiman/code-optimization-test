46db8dc39f9db3524beaaa3c7231c45592ab1c87
steiman <steimansanchez7@gmail.com>
2024-09-21 00:45:15 +0200

    refactor complete for quiz

--- src/Store.java
+++ src/Store.java
@@ -1,26 +1,18 @@
 public class Store {
-    private static final int[] PRICES = {15, 10, 5}; 
-    private static final int[] QUANTITIES = {2, 3, 4}; 
-    private static final int SALES_THRESHOLD = 50; 
-
     public static void main(String[] args) {
-      
-        int totalSales = calculateTotalSales();
-      
+     final int[] PRICES = {15, 10, 5}; 
+     final int[] QUANTITIES = {2, 3, 4}; 
+     final int SALES_THRESHOLD = 50; 
+        // se calcula el total de sales multiplicando el arreglo prices por quantities
+        int totalSales = 0;
+        for (int i = 0; i < PRICES.length; i++) {
+        int totalSales = totalSales + (PRICES[i] * QUANTITIES[i]);
+        
+       // se verifica con el if-else que el programa funcione correctamente 
         if (totalSales > SALES_THRESHOLD) {
             System.out.println("Good sales performance");
         } else {
             System.out.println("Low sales performance");
         }
-    }
-    
-    private static int calculateTotalSales() {
-        int totalSales = 0;
-
-        for (int i = 0; i < PRICES.length; i++) {
-            totalSales += PRICES[i] * QUANTITIES[i];
-        }
-
-        return totalSales;
     }
 }
--- src/TaxCalculation.java
+++ src/TaxCalculation.java
@@ -1,23 +1,25 @@
 
 public class TaxCalculation {
     public static void main(String[] args) {
-        int productos [] = new int [3];
-        productos[0] = 300;
-        productos[1] = 500;
-        productos[2] = 700;
 
-        double productPrice1 = 100;
-        double productPrice2 = 200;
-        double TAX_THRESHOLD = 50;
-        final double TAX_RATE_HIGH = 0.15;
-        final double TAX_RATE_LOW = 0.10;
-        
-        double tax1 = productPrice1 * TAX_RATE_HIGH;
-        double tax2 = productPrice2 * TAX_RATE_LOW;
-        double totalTax = tax1 + tax2;
-        
-        if (TAX_THRESHOLD > 50) {
-            System.out.println("High total tax: " + totalTax);
+        final double TASA_IMPUESTOS_ALTA = 0.15;
+        final double TASA_IMPUESTOS_BAJA = 0.10;
+        final double TASA_IMPUESTO_LIMITE = 50;
+        double[] productosPrecios = { 100, 200 };
+        double[] tasasImpositivas = new double[2];
+        for (int i = 0; i < productosPrecios.length; i++) {
+            if (i == 0) {
+                tasasImpositivas[i] = productosPrecios[i] * TASA_IMPUESTOS_ALTA;
+            }
+            if (i == 1) {
+                tasasImpositivas[i] = productosPrecios[i] * TASA_IMPUESTOS_BAJA;
+            }
+        }
+
+        double totalTasa = tasasImpositivas[0] + tasasImpositivas[1];
+
+        if (totalTasa > TASA_IMPUESTO_LIMITE) {
+            System.out.println("High total tax: " + totalTasa);
         } else {
             System.out.println("Low tax");
         }
